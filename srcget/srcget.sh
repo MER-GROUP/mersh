89579e7dffddb6f4fd3509cf9cebd0ec91e58207
Maxim Ramanenka (Red Alert) <i@mer-group.ru>
2022-09-26 13:38:23 +0300

    add links

--- srcget/srcget.sh
+++ srcget/srcget.sh
@@ -4,3 +4,8 @@
 # Script for downloading and updating sources from github
 # [Скрипт для скачивания и обновления исходников с github]
 
+# read settings
+# [считать настройки]
+for line in $( grep -v '^#' settings.sh ); do
+    echo $line
+done
