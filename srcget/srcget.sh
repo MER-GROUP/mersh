6e88eb9c0107707fd5e169414729695e93c06a52
Maxim Ramanenka (Red Alert) <i@mer-group.ru>
2022-09-27 12:02:58 +0300

    add settings_arr

--- srcget/srcget.sh
+++ srcget/srcget.sh
@@ -4,8 +4,24 @@
 # Script for downloading and updating sources from github
 # [Скрипт для скачивания и обновления исходников с github]
 
-# read settings
-# [считать настройки]
+# settings matrix
+# [матрица настроек]
+declare -A settings_arr
+
+# get settings
+# [получить настройки]
 for line in $( grep -v '^#' settings.sh ); do
     echo $line
+    IFS='=' read -a arr <<< $line
+    settings_arr[${arr[0]}]=${arr[1]}
 done
+
+# # show settings v1
+# # [показать настройки]
+# echo ${settings_arr[@]}
+
+# # show settings v2
+# # [показать настройки]
+# for line in ${settings_arr[@]}; do
+#     echo $line
+# done
