50a75e362d1959fbf27c72d594417557635b9fd7
Maxim Ramanenka (Red Alert) <i@mer-group.ru>
2022-09-27 12:15:49 +0300

    test settings_get

--- srcget/srcget.sh
+++ srcget/srcget.sh
@@ -1,27 +1,54 @@
 #!/bin/bash
-
+# ************************************************************************
 # srcget
+
 # Script for downloading and updating sources from github
 # [Скрипт для скачивания и обновления исходников с github]
+# ************************************************************************
+# global variables
+# [глобальные переменные]
 
 # settings matrix
 # [матрица настроек]
 declare -A settings_arr
+# ************************************************************************
+# function settings_get
 
 # get settings
 # [получить настройки]
-for line in $( grep -v '^#' settings.sh ); do
-    echo $line
-    IFS='=' read -a arr <<< $line
-    settings_arr[${arr[0]}]=${arr[1]}
-done
+settings_get(){
+    # --------------------------------------------------------------------
+    # get settings
+    # [получить настройки]
+    for line in $( grep -v '^#' settings.sh ); do
+        # echo $line
+        IFS='=' read -a arr <<< $line
+        settings_arr[${arr[0]}]=${arr[1]}
+    done
+    # --------------------------------------------------------------------
+    # # show settings v1
+    # # [показать настройки]
+    # echo ${settings_arr[@]}
 
-# # show settings v1
-# # [показать настройки]
-# echo ${settings_arr[@]}
+    # # show settings v2
+    # # [показать настройки]
+    # for line in ${settings_arr[@]}; do
+    #     echo $line
+    # done
+    # --------------------------------------------------------------------
+}
+# ************************************************************************
+# program logic
+# [логика программы]
 
-# # show settings v2
-# # [показать настройки]
-# for line in ${settings_arr[@]}; do
-#     echo $line
-# done
+# get settings
+# [получить настройки]
+settings_get
+# ************************************************************************
+# tests
+echo '###################tests###################'
+echo ${settings_arr[path_src]}
+echo ${settings_arr[file_links]}
+echo ${settings_arr[hist_src]}
+echo '###########################################'
+# ************************************************************************
