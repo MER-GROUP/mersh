82b10289a00945ba5b00c832c49edda40295de0d
Maxim Ramanenka (Red Alert) <i@mer-group.ru>
2022-09-28 13:32:08 +0300

    add check_core_utils

--- srcget/srcget.sh
+++ srcget/srcget.sh
@@ -18,7 +18,7 @@ file_settings=./settings.sh
 # link file
 # [файл ссылок]
 # file_links=./links.sh
-file_links=$( v=$( grep -e 'file_links' $file_settings ); echo ${v#*=} )
+file_links=$( v=$( grep -e 'file_links' ${file_settings} ); echo ${v#*=} )
 
 # settings matrix
 # [матрица настроек]
@@ -26,71 +26,91 @@ declare -A settings_arr
 
 # link matrix
 # [матрица ссылок]
-links_arr=( $( grep -v '^#' $file_links ) )
+links_arr=( $( grep -v '^#' ${file_links} ) )
+# ************************************************************************
+# function check_core_utils
+
+# checking the necessary installed utilities
+# [проверка необходимых установленных утилит]
+check_core_utils(){
+    # --------------------------------------------------------------------
+    # required installed programs [необходимые установленные программы]
+    # arr=( git tar mer) # for test [для тестов]
+    arr=( git tar mer)
+    echo ${arr[@]}
+
+    # checking the necessary installed utilities
+    # [проверка необходимых установленных утилит]
+    for app in ${arr[@]}; do
+        # # test [тест]
+        # app_path=$( which $app )
+        # echo $app_path
+
+        # 0 - the program is installed, 1 and more - not
+        # [0 - программа установленна, 1 и больше - нет]
+        which ${app} > /dev/null
+        bool=$( echo  ${?})
+        if [ 0 -ne ${bool} ]; then
+            echo "for further work, you need to install the program"
+            exit ${bool}
+        fi
+    done
+    # --------------------------------------------------------------------
+}
 # ************************************************************************
 # function settings_get
 
-# get settings
-# [получить настройки]
+# get settings [получить настройки]
 settings_get(){
     # --------------------------------------------------------------------
-    # get settings
-    # [получить настройки]
+    # get settings [получить настройки]
     for line in $( grep -v '^#' $file_settings ); do
         # echo $line
-        IFS='=' read -a arr <<< $line
+        IFS='=' read -a arr <<< ${line}
         settings_arr[${arr[0]}]=${arr[1]}
     done
     # --------------------------------------------------------------------
-    # # show settings v1
-    # # [показать настройки]
+    # # show settings v1 [показать настройки]
     # echo ${settings_arr[@]}
 
-    # # show settings v2
-    # # [показать настройки]
+    # # show settings v2 [показать настройки]
     # for line in ${settings_arr[@]}; do
-    #     echo $line
+    #     echo ${line}
     # done
     # --------------------------------------------------------------------
 }
 # ************************************************************************
 # function check_path
 
-# checking the existence of a directory
-# [проверка существования директории]
+# checking the existence of a directory [проверка существования директории]
 check_path(){ # args: path
     # --------------------------------------------------------------------
-    # data storage directory
-    # [директория хранения данных]
-	path=$1
+    # data storage directory [директория хранения данных]
+	path=${1}
 
     # if the path does not exist then create a directory
     # otherwise, if it is not a directory, then exit the program
     # [если пути не существует то создать директорию]
     # [иначе если это не директория то выйти из программы]
-	if [[ ! -e $path ]]; then
-        echo "Dir is not exist, creating $path"
-        mkdir -p $path
-	elif [[ ! -d $path ]]; then
-        echo "$path is not dir, exiting"
+	if [[ ! -e ${path} ]]; then
+        echo "Dir is not exist, creating ${path}"
+        mkdir -p ${path}
+	elif [[ ! -d ${path} ]]; then
+        echo "${path} is not dir, exiting"
         exit 1
     fi
     # --------------------------------------------------------------------
 }
 # ************************************************************************
-# program logic
-# [логика программы]
+# program logic [логика программы]
 
-# get settings
-# [получить настройки]
+# get settings [получить настройки]
 settings_get
 
-# checking the existence of a directory
-# [проверка существования директории]
+# checking the existence of a directory [проверка существования директории]
 check_path ${settings_arr[path_src]}
 # ************************************************************************
-# tests
-# [тесты]
+# tests [тесты]
 echo '###################tests###################'
 echo '----------settings_get----------'
 echo path_src = ${settings_arr[path_src]}
@@ -102,5 +122,7 @@ echo '----------links_arr----------'
 echo ${links_arr[0]}
 echo ${links_arr[1]}
 echo ${links_arr[2]}
+echo '----------check_core_utils----------'
+check_core_utils
 echo '###########################################'
 # ************************************************************************
