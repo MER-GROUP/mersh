042cf771f9a166fa2eef0618215d27fad8d89540
Maxim Ramanenka (Red Alert) <i@mer-group.ru>
2022-10-06 12:16:52 +0300

    add set-in-math

--- set-in-math/set-in-math.sh
+++ set-in-math/set-in-math.sh
@@ -14,13 +14,45 @@
 # [создание математического множества]
 set-in-math(){ # args: element_1 ... element_N
     # --------------------------------------------------------------------
-    local arr=${@} # local arr=${*}
-    # echo "arr = ${arr}" # test
+    local arr=( ${@} ) # local arr=( ${*} )
+    # echo "arr = ${arr[@]}" # test
+    # echo "len arr = ${#arr[@]}" # test
+
     local set
     # set+=( 1 ); set+=( 2 ) # test
     # echo "set = ${set[@]}" # test
+    # echo "len set = ${#set[@]}" # test
+
+    local index=0
+    # echo "index = ${index}" # test
+    # let index++ # (( index++ )) # test
+    # echo "index = ${index}" # test
+
+    while [[ ${index} -ne ${#arr[@]} ]]; do
+        # echo "index = ${index}" # test
+        local bool=False
+
+        for i in ${set[@]}; do
+            if [[ ${arr[${index}]} -eq ${i} ]]; then
+                bool=True
+                break
+            fi
+        done
+
+        if [[ 'False' == ${bool} ]]; then
+            set+=( ${arr[${index}]} )
+        fi
+
+        let index++
+    done
+
+    # echo "set = ${set[@]}" # test
+    echo ${set[@]}
     # --------------------------------------------------------------------
 }
 # ************************************************************************
-set-in-math "1" "2" "3" "1" "2" "3" "1" "2" "3"
+# tests
+
+# set-in-math # test
+# set-in-math "1" "2" "3" "1" "2" "3" "1" "2" "3" # test
 # ************************************************************************
