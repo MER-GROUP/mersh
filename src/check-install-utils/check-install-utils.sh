791ab093717cb6f1c2babf29d5f77206746e6ab8
Maxim Ramanenka (Red Alert) <i@mer-group.ru>
2022-10-07 14:48:28 +0300

    add check-install-utils

--- src/check-install-utils/check-install-utils.sh
+++ src/check-install-utils/check-install-utils.sh
@@ -14,26 +14,36 @@
 # [Проверка установки утилит]
 check-install-utils(){ # args: program_1 ... program_N
     # --------------------------------------------------------------------
-    # required installed programs [необходимые установленные программы]
-    local arr=( "${*}" )
+    # programs that need to be checked for installation
+    # [программы которые нужно проверить на установку]
+    local arr=( "${@}" ) # local arr=( "${*}" )
     echo ${arr[@]}
 
-    # # checking the necessary installed utilities
-    # # [проверка необходимых установленных утилит]
-    # for app in ${arr[@]}; do
-    #     # # test [тест]
-    #     # local app_path=$( which $app )
-    #     # echo $app_path
+    # programs to install
+    # [программы которые нужно установить]
+    local utils
 
-    #     # 0 - the program is installed, 1 and more - not
-    #     # [0 - программа установленна, 1 и больше - нет]
-    #     which ${app} &> /dev/null
-    #     local bool=$( echo  ${?} )
-    #     if [ 0 -ne ${bool} ]; then
-    #         echo "You need to install the ${app} to continue"
-    #         exit ${bool}
-    #     fi
-    # done
+    # checking the necessary installed utilities
+    # [Проверка установки утилит]
+    for app in ${arr[@]}; do
+        # local app_path=$( which $app ) # test [тест]
+        # echo $app_path # test [тест]
+
+        # 0 - the program is installed, 1 and more - not
+        # [0 - программа установленна, 1 и больше - нет]
+        which ${app} &> /dev/null
+        local bool=$( echo  ${?} )
+        if [ 0 -ne ${bool} ]; then
+            # echo "You need to install the ${app} to continue"
+            # exit ${bool}
+            app+="\n"
+            utils+=( ${app} )
+        fi
+    done
+
+    # You need to install the following programs
+    # [Нужно установить следующие программы]
+    echo -e "You need to install the following programs:\n ${utils[@]}"
     # --------------------------------------------------------------------
 }
 # ************************************************************************
@@ -45,6 +55,9 @@ declare -x -f check-install-utils
 # ************************************************************************
 # tests
 
-check-install-utils # test
-# check-install-utils "1" "2" "3" "1" "2" "3" "1" "2" "3" # test
+# check-install-utils # test
+# utils=( date find git gzip ls mkdir rm tar which ) # test
+# utils=( max date find git gzip ls mkdir rm tar which mer) # test
+# check-install-utils "${utils[@]}" # test
+# check-install-utils "max" "date" "find" "git" "gzip" "ls" "mkdir" "rm" "tar" "which" "mer" # test
 # ************************************************************************
