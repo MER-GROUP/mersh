ce83ff8351842976a435d82117104384aad17dac
Red Alert aka Max Ramanenka <mer-group@yandex.ru>
2022-10-30 20:39:34 +0300

    add sum-in-math

--- src/sum-in-math/sum-in-math.sh
+++ src/sum-in-math/sum-in-math.sh
@@ -8,64 +8,44 @@
 # Script implementation - Max Romanenko (Red Alert) - 2022.
 # Реализация скрипта - Макс Романенко (Red Alert) - 2022г.
 # ************************************************************************
-# function set-in-math
+# function sum-in-math
 
-# creating a mathematical set
-# [создание математического множества]
-set-in-math(){ # args: element_1 ... element_N
+# creating a mathematical sum
+# [создание математической суммы]
+sum-in-math(){ # args: number_1 ... number_N
     # --------------------------------------------------------------------
     local arr=( ${@} ) # local arr=( ${*} )
     # echo "arr = ${arr[@]}" # test
     # echo "len arr = ${#arr[@]}" # test
     # ----------------------------------
-    local set
-    # set+=( 1 ); set+=( 2 ) # test
-    # echo "set = ${set[@]}" # test
-    # echo "len set = ${#set[@]}" # test
+    local sum=0
     # ----------------------------------
-    local index=0
-    # echo "index = ${index}" # test
-    # let index++ # (( index++ )) # test
-    # echo "index = ${index}" # test
+    # СДЕЛАТЬ ПРОЕРКУ ЧТО ВСЕ ВВЕДЕННЫЕ ЭЛЕМЕНТЫ - ЭТО ЧИСЛА !!!!!!!!!!!!!!!!
     # ----------------------------------
-    while [[ ${index} -ne ${#arr[@]} ]]; do
-        # echo "index = ${index}" # test
-        local bool=True
-
-        for i in ${set[@]}; do
-            # if [[ ${arr[${index}]} -eq ${i} ]]; then # for int [для целых чисел]
-            if [[ ${arr[${index}]} == ${i} ]]; then # for str [для строк]
-                bool=False
-                break
-            fi
-        done
-
-        if [[ 'True' == ${bool} ]]; then
-            set+=( ${arr[${index}]} )
-        fi
-
-        let index++ # (( index++ ))
+    for i in ${arr[@]}; do
+        let "sum += i"
+        # (( sum += i )) 
     done
     # ----------------------------------
-    # echo "set = ${set[@]}" # test
-    if [[ 0 -ne ${#set[@]} ]]; then
-        echo ${set[@]}
+    # if [[ 0 -eq "${#}" ]]; then
+    if [[ 0 -ne ${#arr[@]} ]]; then
+        echo ${sum}
     else
-        echo "|-ENG-HELP---------------------------------------------------------|"
-        echo "|  help          : set-in-math - print a mathematical set          |"
-        echo "|  usage         : set-in-math [ sequence of elements ]            |"
-        echo "|  example       : set-in-math 1 2 3 2 3 4 3 4 5                   |"
-        echo "|  output        : 1 2 3 4 5                                       |"
-        echo "|  example       : set-in-math one two one three                   |"
-        echo "|  output        : one two three                                   |"
-        echo "|-RUS-HELP---------------------------------------------------------|"
-        echo "|  помощь        : set-in-math - печатает математическое множество |"
-        echo "|  использование : set-in-math [ последовательность элеменов ]     |"
-        echo "|  пример        : set-in-math 1 2 3 2 3 4 3 4 5                   |"
-        echo "|  вывод         : 1 2 3 4 5                                       |"
-        echo "|  пример        : set-in-math один два один три                   |"
-        echo "|  вывод         : один два три                                    |"
-        echo "|-END--------------------------------------------------------------|"
+        echo "|-ENG-HELP----------------------------------------------------|"
+        echo "|  help          : sum-in-math - creates a mathematical sum   |"
+        echo "|  usage         : sum-in-math [ sequence of numbers ]        |"
+        echo "|  example       : sum-in-math 1 2 3                          |"
+        echo "|  output        : 6                                          |"
+        echo "|  example       : sum-in-math -5 -5                          |"
+        echo "|  output        : -10                                        |"
+        echo "|-RUS-HELP----------------------------------------------------|"
+        echo "|  помощь        : sum-in-math - создаёт математическую сумму |"
+        echo "|  использование : sum-in-math [ последовательность чисел ]   |"
+        echo "|  пример        : sum-in-math 1 2 3                          |"
+        echo "|  вывод         : 6                                          |"
+        echo "|  пример        : sum-in-math -5 -5                          |"
+        echo "|  вывод         : -10                                        |"
+        echo "|-END---------------------------------------------------------|"
     fi
     # --------------------------------------------------------------------
 }
@@ -74,11 +54,14 @@ set-in-math(){ # args: element_1 ... element_N
 
 # function access outside the script
 # [доступ функции вне скрипта]
-declare -x -f set-in-math
+declare -x -f sum-in-math
 # ************************************************************************
 # tests
 
-# set-in-math # test
-# set-in-math "1" "2" "3" "1" "2" "3" "1" "2" "3" # test
-# set-in-math "1" "2" "3" "1" "2" "1" "2" "3" "4" "5" "6" # test
+# sum-in-math # test
+# sum-in-math "1" "2" "3" # test
+# sum-in-math "1" "2" "3" "-1" # test
+# sum-in-math "1" "2" "3" "-1" "-5" # test
+# sum-in-math "1" "2" "3" "-1" "-5" "-5" # test
+# sum-in-math "-5" "-5" # test
 # ************************************************************************
