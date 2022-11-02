253fe55d976b94ef929b646cf94975f37c87b4af
Maxim Ramanenka (Red Alert) <i@mer-group.ru>
2022-11-02 14:57:39 +0300

    update sum-in-math

--- sum-in-math.sh
+++ sum-in-math.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 # ************************************************************************
+# connecting library functions
+# [подключаем функции-библиотеки]
+if [[ 0 -ne $( type check-install-utils &> /dev/null; echo ${?} ) ]]; then
+    source check-install-utils.sh
+fi
+if [[ 0 -ne $( type set-in-math &> /dev/null; echo ${?} ) ]]; then
+    source set-in-math.sh
+fi
+# ************************************************************************
 # connecting the script
 # [подключаем скрипт]
 cd ./src/sum-in-math/
