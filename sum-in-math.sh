bf88a977d34cf0d766c3a512a6179bb667220186
Maxim Ramanenka (Red Alert) <i@mer-group.ru>
2022-11-03 14:35:16 +0300

    update-4 sum-in-math

--- sum-in-math.sh
+++ sum-in-math.sh
@@ -5,9 +5,6 @@
 if [[ 0 -ne $( type check-install-utils &> /dev/null; echo ${?} ) ]]; then
     source check-install-utils.sh
 fi
-if [[ 0 -ne $( type set-in-math &> /dev/null; echo ${?} ) ]]; then
-    source set-in-math.sh
-fi
 # ************************************************************************
 # connecting the script
 # [подключаем скрипт]
