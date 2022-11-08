71f696645f75d210ac2089cee99b92d39d75b810
Maxim Ramanenka (Red Alert) <i@mer-group.ru>
2022-11-08 15:30:29 +0300

    add links for vala

--- src/src-get-from-github/links.sh
+++ src/src-get-from-github/links.sh
@@ -6,13 +6,15 @@
 # topological sort, shortest paths, etc. with graphviz output.
 # altgraph includes some additional usage of Python 2.6+ features 
 # and enhancements related to modulegraph and macholib.
-git@github.com:ronaldoussoren/altgraph.git
+# git@github.com:ronaldoussoren/altgraph.git
+https://github.com/ronaldoussoren/altgraph.git
 
 # networkx
 # NetworkX is a Python package for the creation, 
 # manipulation, and study of the structure, 
 # dynamics, and functions of complex networks.
-git@github.com:networkx/networkx.git
+# git@github.com:networkx/networkx.git
+https://github.com/networkx/networkx.git
 
 # psutil
 # psutil (process and system utilities) is a cross-platform library 
@@ -22,4 +24,17 @@ git@github.com:networkx/networkx.git
 # limiting process resources and management of running processes. 
 # It implements many functionalities offered by classic UNIX command 
 # line tools such as ps, top, iotop, lsof, netstat, ifconfig, free and others.
-git@github.com:giampaolo/psutil.git
+# git@github.com:giampaolo/psutil.git
+https://github.com/giampaolo/psutil.git
+
+# vala
+# Vala is a programming language that aims to bring modern programming
+# language features to GNOME developers without imposing any additional
+# runtime requirements and without using a different ABI compared to
+# applications and libraries written in C.
+# valac, the Vala compiler, is a self-hosting compiler that translates
+# Vala source code into C source and header files. It uses the GObject
+# type system to create classes and interfaces declared in the Vala source
+# code.
+# git@ssh.gitlab.gnome.org:GNOME/vala.git
+https://gitlab.gnome.org/GNOME/vala.git
