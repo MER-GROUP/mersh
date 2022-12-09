cabefff4088a77bbb47dc82401b81c00af62068f
Maxim Ramanenka (Red Alert) <i@mer-group.ru>
2022-12-09 14:54:18 +0300

    update-2 import

--- import.sh
+++ import.sh
@@ -9,13 +9,28 @@
 # [импортирование скриптов в bash/zsh]
 import(){ # NO args
     # --------------------------------------------------------------------
+    # getting folders in a directory [получение файлов в директории]
+    local files_arr=( $( ls -p | grep -v "/$" ) )
+    echo "${files_arr[@]}" # test
+    # ----------------------------------
     # importing scripts into bash/zsh
     # [импортирование скриптов в bash/zsh]
-    source ./check-install-utils.sh
-    source ./mersh.sh
-    source ./set-in-math.sh
-    source ./src-get-from-github.sh
-    source ./sum-in-math.sh
+    # source ./check-install-utils.sh
+    # source ./mersh.sh
+    # source ./set-in-math.sh
+    # source ./src-get-from-github.sh
+    # ...
+    # source ./sum-in-math.sh
+    for lib in ${files_arr[@]}; do
+        if [[ 'import.sh' == ${lib} ]]; then
+            continue
+        elif [[ 'install-in-bash.sh' == ${lib} ]]; then
+            continue
+        elif [[ 'install-in-zsh.sh' == ${lib} ]]; then
+            continue
+        fi
+        source ${lib}
+    done
     # --------------------------------------------------------------------
 }
 # ************************************************************************
