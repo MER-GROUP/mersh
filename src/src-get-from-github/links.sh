3c5d57cedb055d36748fa8250ae8ccb3a480c1ff
Maxim Ramanenka (Red Alert) <i@mer-group.ru>
2022-12-20 15:16:43 +0300

    update links

--- src/src-get-from-github/links.sh
+++ src/src-get-from-github/links.sh
@@ -9,12 +9,67 @@
 # git@github.com:ronaldoussoren/altgraph.git
 https://github.com/ronaldoussoren/altgraph.git
 
+# auto-py-to-exe
+# A .py to .exe converter using a simple graphical interface and PyInstaller in Python.
+https://github.com/brentvollebregt/auto-py-to-exe.git
+
+# Best-README-Template
+# An awesome README template to jumpstart your projects!
+https://github.com/othneildrew/Best-README-Template.git
+
+# buildozer
+# Buildozer is a tool for creating application packages easily.
+https://github.com/kivy/buildozer.git
+
+# clipboard
+# A cross platform clipboard operation library of Python. Works for Windows, Mac and Linux.
+https://github.com/terryyin/clipboard.git
+
+# easygui
+# EasyGUI is a module for very simple, very easy GUI programming in Python. 
+# EasyGUI is different from other GUI libraries in that EasyGUI is NOT event-driven. 
+# Instead, all GUI interactions are invoked by simple function calls.
+https://github.com/robertlugg/easygui.git
+
+# jmtpfs
+# jmtpfs is a FUSE and libmtp based filesystem for accessing MTP (Media Transfer
+# Protocol) devices. It was specifically designed for exchaning files between 
+# Linux (and Mac OS X) systems and newer Android devices that support MTP but not USB Mass 
+# Storage.
+https://github.com/JasonFerrara/jmtpfs.git
+
+# kivy
+# Kivy is an open source, cross-platform Python framework for the development of applications 
+# that make use of innovative, multi-touch user interfaces. The aim is to allow 
+# for quick and easy interaction design and rapid prototyping whilst making your code 
+# reusable and deployable.
+https://github.com/kivy/kivy.git
+
+# KivyMD
+# KivyMD is a collection of Material Design compliant widgets for use with Kivy, 
+# a framework for cross-platform, touch-enabled graphical applications.
+https://github.com/kivymd/KivyMD.git
+
+# mutagen
+# Mutagen is a Python module to handle audio metadata. It supports ASF, FLAC, MP4, 
+# Monkey's Audio, MP3, Musepack, Ogg Opus, Ogg FLAC, Ogg Speex, Ogg Theora, Ogg Vorbis, 
+# True Audio, WavPack, OptimFROG, and AIFF audio files. All versions of ID3v2 are supported, 
+# and all standard ID3v2.4 frames are parsed. It can read Xing headers to accurately calculate 
+# the bitrate and length of MP3s. ID3 and APEv2 tags can be edited regardless of audio format. 
+# It can also manipulate Ogg streams on an individual packet/page level.
+https://github.com/quodlibet/mutagen.git
+
 # networkx
 # NetworkX is a Python package for the creation, 
 # manipulation, and study of the structure, 
 # dynamics, and functions of complex networks.
 # git@github.com:networkx/networkx.git
 https://github.com/networkx/networkx.git
+
+# plyer
+# Plyer is a platform-independent api to use features commonly found on various platforms, 
+# notably mobile ones, in Python.
+https://github.com/kivy/plyer.git
 
 # psutil
 # psutil (process and system utilities) is a cross-platform library 
@@ -26,6 +81,53 @@ https://github.com/networkx/networkx.git
 # line tools such as ps, top, iotop, lsof, netstat, ifconfig, free and others.
 # git@github.com:giampaolo/psutil.git
 https://github.com/giampaolo/psutil.git
+
+# pyinstaller
+# PyInstaller bundles a Python application and all its dependencies into a single package. 
+# The user can run the packaged app without installing a Python interpreter or any modules.
+https://github.com/pyinstaller/pyinstaller.git
+
+# pyjnius
+# A Python module to access Java classes as Python classes using the Java Native Interface (JNI). 
+# Warning: the pypi name is now pyjnius instead of jnius.
+https://github.com/kivy/pyjnius.git
+
+# pyperclip
+# Pyperclip is a cross-platform Python module for copy and paste clipboard functions. 
+# It works with Python 2 and 3.
+https://github.com/asweigart/pyperclip.git
+
+# pyscript
+# PyScript is a framework that allows users to create rich Python applications 
+# in the browser using HTML's interface and the power of Pyodide, WASM, 
+# and modern web technologies.
+https://github.com/pyscript/pyscript.git
+
+# python-chess
+# python-chess is a chess library for Python, with move generation, 
+# move validation, and support for common formats.
+https://github.com/niklasf/python-chess.git
+
+# python-for-android
+# python-for-android is a packaging tool for Python apps on Android. 
+# You can create your own Python distribution including the modules and dependencies you want, 
+# and bundle it in an APK or AAB along with your own code.
+https://github.com/kivy/python-for-android.git
+
+# samba
+# Samba is the standard Windows interoperability suite of programs for Linux and Unix. 
+# Samba is Free Software licensed under the GNU General Public License and 
+# the Samba project is a member of the Software Freedom Conservancy. 
+# Since 1992, Samba has provided secure, stable and fast file and print services 
+# for all clients using the SMB/CIFS protocol, such as all versions of DOS and 
+# Windows, OS/2, Linux and many others. Samba is an important component to 
+# seamlessly integrate Linux/Unix Servers and Desktops into Active Directory environments. 
+# It can function both as a domain controller or as a regular domain member.
+https://github.com/samba-team/samba.git
+
+# simple-crypt
+# Simple, secure encryption and decryption for Python 2.7 and 3.
+https://github.com/andrewcooke/simple-crypt.git
 
 # vala
 # Vala is a programming language that aims to bring modern programming
