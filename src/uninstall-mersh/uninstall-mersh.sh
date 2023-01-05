acaf3e8afb620ad41963bfd17776a90d50a9e450
Maxim Ramanenka (Red Alert) <i@mer-group.ru>
2023-01-05 14:04:58 +0300

    update-17 uninstall-mersh

--- src/uninstall-mersh/uninstall-mersh.sh
+++ src/uninstall-mersh/uninstall-mersh.sh
@@ -30,40 +30,69 @@ uninstall-mersh(){ # NO args
             # getting the current directory
             # [получение текущей директории]
             local current_dir=`pwd`
-            # ----------------------------------
+            # -------------------------------------------------------
+            # we determine the number of mersh scripts
+            # [определяем количество скриптов mersh]
+            # local script_count
+            if [[ -e "${HOME}/.mer-group/mersh" ]] && [[ -d "${HOME}/.mer-group/mersh" ]]; then
+                cd "${HOME}/.mer-group/mersh/"
+                local script_count=( $( ls -p | grep -v "/$" ) )
+                script_count="${#script_count[@]}"
+                let "script_count -= 4" # or
+                # (( script_count -= 4 )) # or
+                # echo "${script_count}" # test
+                cd "${current_dir}"
+            fi
+            # -------------------------------------------------------
             # removing all mersh scripts from a bash session
             # if the file or directory exists
             # [удаление всех скриптов mersh из сессии bash]
             # [если директория существут]
             if [[ -e "${HOME}/.mer-group/mersh" ]] && [[ -d "${HOME}/.mer-group/mersh" ]]; then
-                cd "${HOME}/.mer-group/mersh/"
-                # pwd # test
-                # getting folders in a directory [получение файлов в директории]
-                files_arr=( $( ls -p | grep -v "/$" ) )
-                for lib in ${files_arr[@]}; do
-                    if [[ 'import.sh' == ${lib} ]]; then
-                        continue
-                    elif [[ 'install-in-bash.sh' == ${lib} ]]; then
-                        continue
-                    elif [[ 'install-in-zsh.sh' == ${lib} ]]; then
-                        continue
-                    # elif [[ 'uninstall-mersh.sh' == ${lib} ]]; then
-                    #     continue
-                    elif [[ 'uninstall-mersh_TEMP.sh' == ${lib} ]]; then
-                        continue
-                    else
-                        func=$( echo ${lib%.*} )
-                        # echo "${func}" # test
-                        unset ${func}
-                    fi
-                done
-                cd "${current_dir}"
-                echo "ок -> mersh variables removed"
+                local current_search=`grep "^# mersh vars" ~/.bashrc`
+                current_search=`echo "${current_search%[*}"`
+                if [[ '# mersh vars' == `echo ${current_search}` ]]; then
+                    echo "ок -> mersh variables removed"
+                else
+                    echo "" >> ${HOME}/.bashrc
+                    echo "# mersh vars [переменные mersh]" >> ${HOME}/.bashrc
+
+                    cd "${HOME}/.mer-group/mersh/"
+                    # pwd # test
+
+                    # getting folders in a directory [получение файлов в директории]
+                    files_arr=( $( ls -p | grep -v "/$" ) )
+                    for lib in ${files_arr[@]}; do
+                        if [[ 'import.sh' == ${lib} ]]; then
+                            continue
+                        elif [[ 'install-in-bash.sh' == ${lib} ]]; then
+                            continue
+                        elif [[ 'install-in-zsh.sh' == ${lib} ]]; then
+                            continue
+                        # elif [[ 'uninstall-mersh.sh' == ${lib} ]]; then
+                        #     continue
+                        elif [[ 'uninstall-mersh_TEMP.sh' == ${lib} ]]; then
+                            continue
+                        else
+                            func=$( echo ${lib%.*} )
+                            # echo "${func}" # test
+                            echo "unset ${func}" >> ${HOME}/.bashrc
+                        fi
+                    done
+                    cd "${current_dir}"
+
+                    echo "" >> ${HOME}/.bashrc
+                    echo "ок -> mersh variables removed"
+                fi
             fi
-            # ----------------------------------
+            # -------------------------------------------------------
             # restarting bash
             # [перезапуск bash]
             source ${HOME}/.bashrc
+            # -------------------------------------------------------
+
+
+
             # -------------------------------------------------------
         else
             echo "!!!!!!!!!! SPRAVKA !!!!!!!!!!"
